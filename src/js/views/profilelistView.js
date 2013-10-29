a5f3ddc539cf6b665e7a4c93fd1d1f799f62b7c4
Greg Hild <grhild@gmail.com>
2013-10-29 12:28:20 -0500

    Remove unneccessary words at bottom of index page, add broken javascript to populate nearby user page via template.

--- src/js/views/profilelistView.js
+++ src/js/views/profilelistView.js
@@ -5,15 +5,23 @@ window.ProfileListView = Backbone.View.extend({
 
 
   initialize: function(options) {
-    console.log('ProfileListView is initiated!');
+    this.listenTo(this.collection, 'add', this.addProfile);
   },
 
-  template: _.getTemplate('profilelist'),
+  template: _.getTemplate('userInfo'),
+
+  addProfile: function (profile) {
+    profile.view = new ProfileView({ model: profile });
+  
+    task.view.render();
+    $(this.el).append( profile.view.el );
+  },
 
   render: function () {
     var userListHtml = this.template(this.model.toJSON());
     $(this.el).html(userListHtml);
 
+    this.collection.each(this.addProfile, this);
   }
   
 });
