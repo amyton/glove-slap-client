33a9ba583e6e40fb52092203802b4d1d064f1f5a
Greg Hild <grhild@gmail.com>
2013-11-06 00:28:32 -0600

    Build using staticmatic and update index js with airship js replacing old phonegap gcm js.

--- src/js/index.js
+++ src/js/index.js
@@ -17,73 +17,102 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  // Application Constructor
+  initialize: function() {
+    this.bindEvents();
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicity call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
+    app.receivedEvent('deviceready');
+  },
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+    console.log('Received Event: ' + id);
 
-        // result contains any message sent from the plugin call
-        var successHandler = function(result) {
-          alert('Callback Success! Result = ' + result);
-        };
-        var errorHandler = function(error) {
-          alert(error);
-        };
+    // result contains any message sent from the plugin call
+    var successHandler = function(result) {
+      alert('Callback Success! Result = ' + result);
+    };
+    var errorHandler = function(error) {
+      alert(error);
+    };
 
 
-        var pushNotification = window.plugins.pushNotification;
-        pushNotification.register(successHandler, errorHandler,{"senderID":"192711177869","ecb":"app.onNotificationGCM"});
-    },
+    // Urban Airship with phonegap code. Registers app to receive APID from Urban Airship
+    push = window.plugins.pushNotification;
 
-    onNotificationGCM: function(e) {
-      switch( e.event )
-      {
-        case 'registered':
-          if ( e.regid.length > 0 )
-          {
-              console.log("Regid " + e.regid);
-              alert('registration id = ' + e.regid);
-          }
-        break;
+    // Callback for when a device has registered with Urban Airship.
+    // https://docs.urbanairship.com/display/DOCS/Server%3A+Android+Push+API#ServerAndroidPushAPI-Registration
+    push.registerEvent('registration', function (error, id) {
+        if (error) {
+            console.log('there was an error registering for push notifications');
+        } else {
+            console.log("Registered with ID: " + id);
+        } 
+    });
 
-        case 'message':
-          // this is the actual push notification. its format depends on the data model from the push server
-          alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-        break;
+    // Callback for when the app is running, and receives a push.
+    push.registerEvent('push', function (push) {
+        console.log("Got push: " + push.message)
+    });
 
-        case 'error':
-          alert('GCM error = '+e.msg);
-        break;
+    // Check if push is enabled
+    push.isPushEnabled(function (enabled) {
+        if (enabled) {
+            console.log("Push is enabled! Fire away!");
+        }
+    })
 
-        default:
-          alert('An unknown GCM event has occurred');
-            break;
-      }
-    }
+
+  //     var pushNotification = window.plugins.pushNotification;
+  //     pushNotification.register(successHandler, errorHandler,{"senderID":"192711177869","ecb":"app.onNotificationGCM"});
+  }
+
+  // onNotificationGCM: function(e) {
+  //   switch( e.event )
+  //   {
+  //     case 'registered':
+  //       if ( e.regid.length > 0 )
+  //       {
+  //           console.log("Regid " + e.regid);
+  //           alert('registration id = ' + e.regid);
+  //       }
+  //     break;
+
+  //     case 'message':
+  //       // this is the actual push notification. its format depends on the data model from the push server
+  //       alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+  //     break;
+
+  //     case 'error':
+  //       alert('GCM error = '+e.msg);
+  //     break;
+
+  //     default:
+  //       alert('An unknown GCM event has occurred');
+  //         break;
+  //   }
+  // }
+
+
+
 };
 
 
